ef11d76dadbd28d7831c6a0dafad60333f42a05b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-23 11:54:13 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,18 +11,17 @@ class Song < ActiveRecord::Base
      self.artist = Artist.find_or_create_by(name: name)
   end
 
-  def genre_id
+  def genre_name
     self.genre ? self.genre.name : nil
   end
 
-  def genre_id=(name)
+  def genre_name=(name)
      self.genre = Genre.find_or_create_by(name: name)
   end
 
   def note_contents=(contents)
     contents.each do |content|
-      note = Note.find_or_create_by(content: content) unless content
-      self.notes << note
+      self.notes << Note.find_or_create_by(content: content) unless content.empty?
     end
   end
 
