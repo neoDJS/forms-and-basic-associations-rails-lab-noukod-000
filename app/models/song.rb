793873cbe1d6ae29429fc311be6c92bf20167ad4
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-23 11:42:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -21,7 +21,7 @@ class Song < ActiveRecord::Base
 
   def note_contents=(contents)
     contents.each do |content|
-      self.notes << Note.find_or_create_by(content: content) unless content
+      self.notes << Note.find_or_create_by(content: content) unless content.empty?
     end
   end
 
